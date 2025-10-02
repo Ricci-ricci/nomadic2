bae144f6661e717e5a4893a45f02dbd6bcc60962
jckylalaina <jlrandrianarivony@gmail.com>
2025-10-02 12:56:43 +0300

    image-again

--- app/screencomponents/best/best.js
+++ app/screencomponents/best/best.js
@@ -13,8 +13,8 @@ export default function BestDestination() {
       "The best island in Madagascar \n\n\nThe Ultimate Island Escape in Madagascar \nLooking for the Best Island in Madagascar? Discover Nosy Be!\nNosy Be, located off Madagascar’s northwestern coast, is the country’s top tourist destination.\nIn the southeast of the island, a hike through Lokobe National Park will take you deep into lush forest, where you’ll encounter a fascinating variety of reptiles and lemurs. The island’s capital, Hell-Ville, is known for its charming French colonial architecture and its bustling local market perfect for picking up souvenirs.\nLemuria Land is another highlight, home to many species of lemurs and other native wildlife.\nNosy Be is also surrounded by beautiful smaller islands like Nosy Iranja, Nosy Sakatia, and Nosy Komba, each offering unique marine fauna and flora, ideal for snorkeling and diving.\nUnwind with unforgettable moments at the Blue Lagoon, where white sandy beaches and crystal-clear waters await. For a truly breathtaking view, don’t miss the hike up Mont Passot, which offers a panoramic lookout over crater lakes and lush landscapes.\nSo don’t hesitate, join our team and come explore the unforgettable beauty of Madagascar! Discover Nosy Be!",
     image: `/destination/The best Island/principal.jpeg`,
     thumbnail: [
-      `/destination/The best Island/second1.jpg`,
-      `/destination/The best Island/second2.jpg`,
+      `/destination/ThebestIsland/second1.jpg`,
+      `/destination/ThebestIsland/second2.jpg`,
     ],
   };
 
